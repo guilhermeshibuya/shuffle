60e74e0ad5cd494e40e7eee0e0f57751b83ab953
Guilherme Yudi Shibuya <gyudishibuya@gmail.com>
2023-09-28 09:00:34 -0300

    add: search screen import

--- src/screens/Search/index.js
+++ src/screens/Search/index.js
@@ -1,4 +1,5 @@
-import { View } from "react-native";
+import { TextInput, View } from "react-native";
+import styles from "./styles";
 
 export default function SearchScreen() {
   return <View></View>;
